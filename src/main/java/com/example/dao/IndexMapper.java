c33a6a37854c2de31dbb6906ea0878b47490e254
xuan xie <963113206@qq.com>
2022-09-20 16:41:31 +0800

    完成后端登陆接口的实现

--- src/main/java/com/example/dao/IndexMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.entity.NocvData;
-
-public interface IndexMapper extends BaseMapper<NocvData> {
-}
--- src/main/java/com/example/service/IndexService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.entity.NocvData;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface IndexService extends IService<NocvData> {
-}
